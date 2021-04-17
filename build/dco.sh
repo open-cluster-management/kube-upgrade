42367ef4043b2be20f08836ee4d0003a293ef04b
Dominique Vernier <dvernier@redhat.com>
2021-04-17 08:54:01 -0400

    Add message
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- build/dco.sh
+++ build/dco.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Copyright Contributors to the Open Cluster Management project
 # set -x
+set -e
 BRANCH=$1
-COMMITS=$(git log main..${BRANCH} | grep commit | cut -d ' ' -f2)
+echo "Check all commit signed on branch: ${BRANCH}"
+COMMITS=$(git log origin/main..${BRANCH} | grep commit | cut -d ' ' -f2)
 for c in ${COMMITS}
 do
   COMMIT=$(git show ${c} -q)
