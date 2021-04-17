9c774ff653b4ca40075ba9ec820cf9e5bcf1e750
Dominique Vernier <dvernier@redhat.com>
2021-04-17 08:47:01 -0400

    extract branch name
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- build/dco.sh
+++ build/dco.sh
@@ -2,9 +2,7 @@
 # Copyright Contributors to the Open Cluster Management project
 # set -x
 BRANCH=$1
-echo "BRANCH: ${BRANCH}"
-echo "BRANCH_NAME: ${BRANCH_NAME}"
-COMMITS=$(git log origin/main..${BRANCH_NAME} | grep commit | cut -d ' ' -f2)
+COMMITS=$(git log origin/main..${BRANCH} | grep commit | cut -d ' ' -f2)
 for c in ${COMMITS}
 do
   COMMIT=$(git show ${c} -q)
