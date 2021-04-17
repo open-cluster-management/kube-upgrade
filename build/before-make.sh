b2905c9a80890123ef4c21118f3562cfa49b4b68
Dominique Vernier <dvernier@redhat.com>
2021-04-17 11:03:43 -0400

    test6
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- build/before-make.sh
+++ build/before-make.sh
@@ -4,4 +4,4 @@
 # Copyright Contributors to the Open Cluster Management project
 
 ln -sf ../../build/git-hooks/pre-commit .git/hooks/pre-commit
-ln -sf ../../build/git-hooks/post-commit .git/hooks/post-commit
+ln -sf ../../build/git-hooks/commit-msg .git/hooks/commit-msg
